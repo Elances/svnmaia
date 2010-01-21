bad906c0260b68d2aa1a36369f59ea70cc5d9eef
xuejiang78 <xuejiang78@61e5e0dc-9616-11de-a4d2-b33cf26bcc48>
2010-01-21 01:28:51 +0000

    调整格式，删除空行

--- extension/index.php
+++ extension/index.php
@@ -48,7 +48,7 @@ br{clear:both;}
  </div>
  <h3>权限申请</h3>
   <div class='st'>
-<br><a href='./autopriv/setting.php'>自动审批权限设置</a>
+<a href='./autopriv/setting.php'>自动审批权限设置</a>
 <br><a href='./autopriv/rtpriv.php' target='_blank'>权限申请</a>
   </div>
 <h3>自定义工具集</h3>
