45c197af983b39ebf1dc7db1801643ab270dbe73
xuejiang78 <xuejiang78@61e5e0dc-9616-11de-a4d2-b33cf26bcc48>
2010-01-29 07:41:29 +0000

    调整文案，增加权限二字。

--- extension/autopriv/rtpriv.php
+++ extension/autopriv/rtpriv.php
@@ -20,7 +20,7 @@ fieldset{border:2px solid #A4CDF2;padding:20px;background:#FFFFFF;}
    		
    <table valign=top>
  <tr><td>申请的url:<input type=text name='wurl' size='45'  onBlur="checkurl();"></td>
-       <td>&nbsp;&nbsp;<select name="wpriv"><option value='r' label='只读'>只读</option>
+       <td>&nbsp;&nbsp;权限:<select name="wpriv"><option value='r' label='只读'>只读</option>
 <option value='w' label='读写'>读写</option>
 </select></td></tr>
   <tr><td colspan=3><label id='urltip' style='color:red;font-size:12px;'></label></td></tr>
