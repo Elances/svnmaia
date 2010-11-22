a802540600bc27a7153b73864f229983966e1239
xuejiang78 <xuejiang78@61e5e0dc-9616-11de-a4d2-b33cf26bcc48>
2010-11-22 07:19:34 +0000

    添加【代码修改订阅功能】，用户可删除自己的订阅。

---
+++ extension/monitor_modify.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+include('../include/charset.php');
+error_reporting(0);
+/*
+   文件名：monitor_modify.php
+   功能：删除、修改svn监控。
+   输入：
+   输出：
+   逻辑：
+*/
+include('../../../config.inc');
+include('../include/dbconnect.php');
+$id=$_GET['id'];
+$action=$_GET['action'];
+if(!is_numeric($id))
+{
+	echo "非法";
+	exit;
+}
+if(empty($id))
+{
+	echo " <script>window.alert(\"输入信息不全!\")</script>";
+  echo " <a href='javascript:history.back()'>点击这里返回</a>";
+  echo " <script>setTimeout('document.location.href=\"javascript:history.go(-1)\"',5)</script>";
+  exit;
+	
+}
+$user_id=$_SESSION['uid'];
+if('del' == $action)
+{
+	$query="delete from monitor_user where user_id=$user_id and id=$id";
+	mysql_query($query);
+	echo "<script>alert('该订阅已删除!')</script>";
+	echo " <script>setTimeout('document.location.href=\"svn_monitor.php\"',5)</script>";
+}
+
