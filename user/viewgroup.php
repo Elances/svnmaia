61651d595c8a7e552991bec300acb43aa0cd848b
xuejiang78 <xuejiang78@61e5e0dc-9616-11de-a4d2-b33cf26bcc48>
2010-12-24 09:27:53 +0000

    【组管理】修改组管理员判断逻辑，非目录管理员也可以管理组。

--- user/viewgroup.php
+++ user/viewgroup.php
@@ -99,21 +99,19 @@ function checkinput($str)
 }
 function isadmin($gid)
 {
- if ((isset($_SESSION['username']))and($_SESSION['role']=="admin")){ 
+  if (! isset($_SESSION['username'])) return false;
+  if ($_SESSION['role']=="admin"){ 
 	return true;
- }
+  }
  
- if ((isset($_SESSION['username']))and($_SESSION['role']=="diradmin")){ 
-	$sql="select user_id from svnauth_user where user_name='".$_SESSION['username']."'";
-	$t_res=mysql_query($sql);
-	$row= mysql_fetch_array($t_res, MYSQL_BOTH);
-	$t_uid=$row['user_id'];
-	$sql="select isowner from svnauth_groupuser where group_id=$gid and user_id=$t_uid";
-	$t_res=mysql_query($sql);
-	$row= mysql_fetch_array($t_res, MYSQL_BOTH);
-	$t_isowner=trim($row['isowner']);
-	if(!empty($t_isowner))return true;
 
+ $t_uid=$_SESSION['uid'];
+ $sql="select isowner from svnauth_groupuser where group_id=$gid and user_id=$t_uid";
+ $t_res=mysql_query($sql);
+ $row= mysql_fetch_array($t_res, MYSQL_BOTH);
+ $t_isowner=trim($row['isowner']);
+ if(!empty($t_isowner))return true;
+ if ($_SESSION['role']=="diradmin"){ 
 	$query="select repository,path from svnauth_g_permission where group_id=$gid";
 	$result=mysql_query($query);
 	$t_allmatch=true;
